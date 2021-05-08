87f929012f7bf7ac8c0894b2c23faae6742df313
Sok4h <55671660+Sok4h@users.noreply.github.com>
2021-05-08 01:44:15 -0500

    number display working

--- app/src/main/java/com/sokah/calculator/MainActivity.kt
+++ app/src/main/java/com/sokah/calculator/MainActivity.kt
@@ -3,20 +3,43 @@ package com.sokah.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric = false;
+    var lastDot = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
 
-    fun onDigit(view: View) {
+    fun Ondigit(view: View) {
 
-    Toast.makeText(this,"Button Works",Toast.LENGTH_SHORT).show()
+        txtInput.append((view as Button).text)
+        lastNumeric = true
+
 
     }
 
+    fun Clear(view: View){
 
+        txtInput.text=""
+        lastNumeric = false
+        lastDot = false
+
+    }
+    fun OnDecimalPoint(view: View){
+
+        if(lastNumeric &&!lastDot) {
+
+            txtInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+
+    }
 }
